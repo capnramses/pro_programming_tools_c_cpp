efe7af9ab9d4fd0ee7ccab67ef6e5538275280bd
Anton Gerdelan <antonofnote@gmail.com>
2020-03-11 14:06:44 +0000

    added && trick to assertion

--- ch1_assertions/main.c
+++ ch1_assertions/main.c
@@ -18,7 +18,7 @@ str_index - Byte index in str to change to `\0`.
 */
 void my_str_truncate( char* str, int str_len, int str_index ) {
   // Abort program if str was NULL.
-  assert( str );
+  assert( str && "str parameter was NULL" );
   // Abort if str_index was outside of allowed range
   assert( str_index >= 0 && str_index < str_len );
 
@@ -28,8 +28,8 @@ void my_str_truncate( char* str, int str_len, int str_index ) {
 int main() {
   char name[10];
   strncpy( name, "anton", 10 );
-  int new_length = 3; // try changing to a number >= 10
-  my_str_truncate( name, 10, new_length );
+  int new_length = 3;                      // try changing to a number >= 10
+  my_str_truncate( name, 10, new_length ); // try replacing name with NULL
   // should print "ant"
   printf( "truncated: %s\n", name );
 
