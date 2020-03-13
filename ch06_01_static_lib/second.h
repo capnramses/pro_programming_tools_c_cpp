c6100e926aab5c0a6f1716bd1b7a09f10a66877b
Anton Gerdelan <antonofnote@gmail.com>
2020-03-13 22:34:04 +0000

    cpp/c guard for vs

--- ch06_01_static_lib/second.h
+++ ch06_01_static_lib/second.h
@@ -7,6 +7,15 @@ Repository: https://github.com/capnramses/pro_programming_tools_c_cpp
 #ifndef SECOND_H_
 #define SECOND_H_
 
+/* Make sure a C++ compiler treats these C function names as C symbols. */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void second();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // you can replace all three #...  with just `#pragma once` at the top
